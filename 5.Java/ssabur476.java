8005d1421526c912d091de27f276508b762d3bf0
ssabur476 <68954445+ssabur476@users.noreply.github.com>
2020-10-23 22:14:40 +0800

    Program to inout odd and even numbers

---
+++ 5.Java/ssabur476.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Lab1_task3 {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int nums = scanner.nextInt();
+        System.out.println(nums);
+        if (nums%2==0)
+        System.out.print("This is an even number");
+        else System.out.print("This is an even number");
+}
+}
